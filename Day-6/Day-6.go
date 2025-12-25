981a514b1a4f6686d48d3323fc23667d7febe5c4
Shudhesh Velusamy <shudheshkibhakt@gmail.com>
2025-12-25 23:22:17 +0530

    Solved Part One

--- Day-6/Day-6.go
+++ Day-6/Day-6.go
@@ -2,9 +2,28 @@ package main
 
 import (
 	"fmt"
-	"github.com/Shuddown/Advent-of-Code/utils"
 	"io"
+	"strconv"
+	"strings"
+
+	"github.com/Shuddown/Advent-of-Code/utils"
 )
+
+func performOp(operator rune, nums ...int) int {
+	if operator == '*' {
+		product := 1
+		for _, num := range nums {
+			product *= num
+		}
+		return product
+	} else {
+		sum := 0
+		for _, num := range nums {
+			sum += num
+		}
+		return sum
+	}
+}
 
 func main() {
 	inputFile, err := utils.GetInput(6)
@@ -12,6 +31,33 @@ func main() {
 	defer inputFile.Close()
 	b, err := io.ReadAll(inputFile)
 	utils.HandleError(err)
-	fmt.Println(string(b))
-}
 
+	s := string(b)
+	numsPerProblem := strings.Count(s, "\n") - 1
+	delimiter := " \n"
+	numsAndOperators := strings.FieldsFunc(s, func(r rune) bool {
+		return strings.ContainsRune(delimiter, r)
+	})
+
+	l := len(numsAndOperators)
+	numProblems := l / (numsPerProblem + 1)
+	stringNums := numsAndOperators[:numProblems*numsPerProblem]
+	operators := numsAndOperators[numProblems*numsPerProblem:]
+
+	nums := make([]int, len(stringNums))
+	for index := range stringNums {
+		num, err := strconv.Atoi(stringNums[index])
+		utils.HandleError(err)
+		nums[index] = num
+	}
+	sum := 0
+	for i := range numProblems {
+		problem := make([]int, 0, numsPerProblem)
+		for j := range numsPerProblem {
+			problem = append(problem, nums[i+numProblems*j])
+		}
+		sum += performOp([]rune(operators[i])[0], problem...)
+	}
+	fmt.Println(sum)
+
+}
